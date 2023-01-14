069d09995944362403a7dd3f34b2b9066a05f45f
Renee Mumbi <1723renee@gmail.com>
2023-01-14 22:02:17 +0300

    Add useState(s) for the various form fields

---
+++ src/components/TransactionsForm.jsx
@@ -0,0 +1,9 @@
+import React, {useState} from "react";
+
+function TransactionsForm(){
+
+    const [date, setDate]= useState("")
+    const [description, setDescription] = useState("")
+    const [category, setCategory]= useState("")
+    const [amount, setAmount] = useState("")
+}
