3dd7fea8af488323b563b40872216df12e05b6f3
Renee Mumbi <1723renee@gmail.com>
2023-01-16 12:47:20 +0300

    create handleSearch function

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,12 +1,17 @@
 import React from "react";
 
 function Search({searchTerm, setSearch}) {
+  function handleSearch(e){
+    setSearch(e.target.value);
+
+  }
   return (
     <div className="ui large fluid icon input">
       <input
         type="text"
         placeholder="Search your Recent Transactions"
-        onChange={() => console.log("Searching...")}
+        value={searchTerm}
+        onChange={handleSearch}
       />
       <i className="circular search link icon"></i>
     </div>
