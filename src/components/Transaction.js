b1bae559dafa7ca67dfef4a444fa6839aedc6b1f
Renee Mumbi <1723renee@gmail.com>
2023-01-17 17:20:08 +0300

    Add Delete button to the table

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -7,7 +7,7 @@ function Transaction({date,description,category, amount}) {
       <td>{description}</td>
       <td>{category}</td>
       <td>{amount}</td>
-      <td></td>
+      <td>{<button onClick={handleDelete}>Delete</button>}</td>
     </tr>
   );
 }
